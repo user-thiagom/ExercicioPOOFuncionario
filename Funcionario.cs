90a114500243ca5d2c6bc4609ed8d65514275ddb
Thiago Mateus <thiagomateus123@hotmail.com>
2022-01-26 16:00:18 -0300

    Finalização da Atividade

---
+++ Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPOOFuncionario
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double novoSalario = ((porcentagem / 100) + 1) * SalarioBruto;
+            SalarioBruto = novoSalario;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ExercicioPOOFuncionario;
+
+Funcionario funcionario = new Funcionario();
+
+Console.WriteLine("Nome: ");
+funcionario.Nome = Console.ReadLine();
+
+Console.WriteLine("Salario Bruto: ");
+funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Imposto: ");
+funcionario.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Funcionario: {funcionario.Nome}, $ {funcionario.SalarioLiquido()}");
+
+Console.WriteLine("Digite a porcentagem para aumentar o salário: ");
+funcionario.AumentarSalario(double.Parse(Console.ReadLine()));
+
+Console.WriteLine($"Dados atualizados: {funcionario.Nome}, $ {funcionario.SalarioLiquido()}");
